16e04cd7ef7c1090343f5a954e07443810f705b5
Rohan Goel <rohang4@illinois.edu>
2020-11-30 03:42:35 +0400

    adding data extraction file

---
+++ dataextraction.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import json
+
+def extract_data(filename):
+    data = []
+    with open(filename,'r') as file:
+        for line in file:
+            data.append(line)
+    return data
+
+def parse_json(data, train):
+    df = None
+    if train:
+        df = pd.DataFrame(columns = ['label', 'response', 'context'])
+    else:
+        df = pd.DataFrame(columns = ['id', 'response', 'context'])
+    for i in data:
+        dict = json.loads(i)
+        l = list(dict.values())
+        context = l.pop()
+        context_s = context[0]
+        for i in range(1, len(context)):
+            context_s += " " + context[i]
+        l.append(context_s)
+        tmp = pd.DataFrame([l], columns = list(df.columns))
+        df = df.append(tmp)
+    return df
